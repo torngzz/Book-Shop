35b0e53dea22303b6d0b525420246418ce10d799
Chhotrakunkhunphovodon <chukizz904@gmail.com>
2024-07-17 09:01:20 +0700

    second commit

---
+++ src/test/java/com/aub/e_shop/EShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aub.e_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
